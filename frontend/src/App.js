0b29829edf5e7a6cdb7132b4e0417ff0b176b9c1
eltonlazzarin <elton.lazzarin@outlook.com>
2019-09-19 18:02:16 -0300

    project files

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import Header from './components/Header'
+import Routes from './routes'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import api from '../services/api.js';
+import io from 'socket.io-client';
+
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+
+class Feed extends Component {
+
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount(){
+        this.registerToSocket();
+
+        const response = await api.get('posts');
+
+        this.setState({feed: response.data});
+    }
+
+    registerToSocket = () =>{
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost =>{
+            this.setState({feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                        post._id === likedPost._id ? likedPost : post
+                    )
+            });
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    render(){
+        return (
+           <section id="post-list">
+               {this.state.feed.map(post => ( /* map goes through each of the posts */
+                <article key={post._id}>
+                   <header>
+                       <div className="user-info">
+                        <span>{post.author}</span>
+                        <span className="place">{post.place}</span>
+                       </div>
+
+                       <img src={more} alt="More"/>
+                   </header>
+
+                   <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+
+                   <footer>
+                        <div className="actions">
+                        <button type="button" onClick={() => this.handleLike(post._id)}>
+                           <img src={like} alt=""/>
+                        </button>
+                           <img src={comment} alt=""/>
+                           <img src={send} alt=""/>
+                        </div>
+
+                           <strong>
+                               {post.likes} likes
+                           </strong>
+                           <p>
+                               {post.description} 
+                               <span>{post.hashtags}</span>
+                           </p>
+                       
+                   </footer>
+               </article>
+               ))} 
+           </section>
+        );
+    }
+}
+
+export default Feed;
