ece2e9095a9197f6570d92ade34a59730049d57a
eltonlazzarin <elton.lazzarin@outlook.com>
2019-09-19 18:15:52 -0300

    project files

---
+++ mobile/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,57 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import com.mobile.BuildConfig;
+import com.mobile.R;
+
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// react-native-image-picker
+import com.imagepicker.ImagePickerPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this.reactNativeHost = reactNativeHost;
+  }
+
+  public PackageList(Application application) {
+    this.reactNativeHost = null;
+    this.application = application;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(),
+      new RNGestureHandlerPackage(),
+      new ImagePickerPackage()
+    ));
+  }
+}
